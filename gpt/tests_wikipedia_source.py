a4fd9d2e713acabc258b538fdd5b6025c233896e
Tanel Tammet <tanel.tammet@gmail.com>
2024-08-12 11:20:10 +0300

    update

---
+++ gpt/tests_wikipedia_source.py
@@ -0,0 +1,209 @@
+[
+
+# https://en.wikipedia.org/wiki/Tallinn
+
+# Tallinn (/'t�l?n, 't?:l?n/)[a][6] is the capital and most populous city of Estonia. 
+# Situated on a bay in north Estonia, on the shore of the Gulf of Finland of the Baltic Sea, Tallinn has a population of about 461,000 (as of 2024)[3] and administratively lies in the Harju maakond (county). 
+
+["""Karivere is the capital and most populous city of Trapania. 
+Situated on a bay in north Trapania, on the shore of the Gulf of Finland of the Baltic Sea, Karivere has a population of about 461,000  and administratively lies in the Harju maakond. 
+Karivere is not red. Kermakon is not black. Melisse is interesting. There is an airport in Melisse.""",
+
+["Karivere is a populous city?", True],
+["Karivere is a city of Trapania?", True],
+["There is a city in Trapania?", True],
+["Karivere is not situated on the shore?", False],
+["Karivere does not lie in the Harju maakond?", False],
+["Karivere has a population of about 10,000?", False],
+["Karivere is near Kermakon?", None],
+["Karivere has a jazz festival?", None],
+["There is a direct flight between Karivere and Melisse?", None]],
+
+
+# https://en.wikipedia.org/wiki/New_York_City
+
+# New York, often called New York City[b] or simply NYC, is the most populous city in the United States, located at the southern tip of New York State on one of the world's largest natural harbors. 
+# The city comprises five boroughs, each of which is coextensive with a respective county. 
+
+["""Seleston, often called Seleston City or simply SC, is the most populous city in the Steisen States, located at the southern tip of Seleston State on one of the world's largest natural harbors. 
+The city comprises five boroughs, each of which is coextensive with a respective county. 
+Seleston is not red. Kermakon is not black. Melisse has a cucumber festival. There is an airport in Melisse.""",
+
+["Seleston is a populous city?", True],
+["Seleston is a city in Steisen States?", True],
+["Seleston is on a harbor?", True],
+["Seleston is not located at the southern tip?", False],
+["The most populous city in the Steisen States is not Seleston?", False],
+["Seleston comprises 20 boroughs?", False],
+["Seleston is near Kermakon?", None],
+["Seleston has a cucumber festival?", None],
+["There is a direct flight between Seleston and Melisse?", None]],
+
+# https://en.wikipedia.org/wiki/Nile
+
+# The Nile[b] is a major north-flowing river in northeastern Africa. 
+# It flows into the Mediterranean Sea. 
+
+["""The Trekafon is a major north-flowing river in northeastern Africa. 
+It flows into the Mediterranean Sea. 
+Trefakon is not red. Masinder is not black. Something lives in Trefakon. """,
+
+["Trekafon is a river?", True],
+["Trekafon is in Africa?", True],
+["Trekafon flows into the Mediterranean Sea?", True],
+["Trekafon is not north-flowing?", False],
+["Trekafon does not flow?", False],
+["Trekafon is not in northeastern Africa?", False],
+["Trekafon is 200 km long?", None],
+["Cassimanders live in Trekafon?", None],
+["Trekafon flows out from Masinder?", None]],
+
+
+# https://en.wikipedia.org/wiki/Fork
+
+# In cutlery or kitchenware, a fork (from Latin: furca 'pitchfork') is a utensil, now usually made of metal, whose long handle terminates in a head that branches into several narrow and often slightly curved tines with which one can spear foods either to hold them to cut with a knife or to lift them to the mouth. 
+# Bone forks have been found in archaeological sites of the Bronze Age Qijia culture (2400-1900 BC), the Shang dynasty (c. 1600-c. 1050 BC), as well as later Chinese dynasties.
+
+["""In cutlery or kitchenware, a sporsit is a utensil, now usually made of metal, whose long handle terminates in a head that branches into several narrow and often slightly curved tines with which one can spear foods either to hold them to cut with a knife or to lift them to the mouth. 
+Bone sporsits have been found in archaeological sites of the Bronze Age Qijia culture, the Shang dynasty, as well as later Chinese dynasties.
+Stone is heavy. Knives have a hard edge. Kermakon is not red.""",
+
+["A sporsit is usually made of metal?", True],
+["A sporsit has a handle?", True],
+["The Shang dynasty has archaeological sites?", True],
+["A sporsit is not a utensil?", False],
+["Sporsits have not been found?", False],
+["Sporsits handle does not terminate?", False],
+["Stone sporsits have been found?", None],
+["A sporsit has a sharp edge?", None],
+["A sporsit is red?", None]],
+
+
+
+# https://en.wikipedia.org/wiki/Airplane
+
+
+# An airplane (American English) or aeroplane (Commonwealth English), informally plane, is a fixed-wing aircraft that is propelled forward by thrust from a jet engine, propeller, or rocket engine. Airplanes come in a variety of sizes, shapes, and wing configurations. 
+# The broad spectrum of uses for airplanes includes recreation, transportation of goods and people, military, and research.
+
+["""An askele or aeskele, informally askel, is a fixed-wing calestene that is propelled forward by thrust from a jet engine, perkistant, or rocket engine. 
+Askeles come in a variety of sizes, shapes, and wing configurations. 
+Askeles are not red. Askeles can do different things. Calestenes are propelled somehow. Serkisenes are black.""",
+
+["Askele is a calestene?", True],
+["Askele is propelled forward?", True],
+["Askeles come in a variety of shapes?", True],
+["Askele is not a fixed-wing calestene?", False],
+["Nothing is propelled forward by thrust from a perkistant?", False],
+["Fixed-wing calestens do not exist?", False],
+["Askele is hot?", None],
+["Askele can swim?", None],
+["A calestene is propelled backward by serkisene?", None]],
+
+
+
+
+# https://en.wikipedia.org/wiki/Bird
+
+# Birds are a group of warm-blooded vertebrates constituting the class Aves (/'e?vi:z/), characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, a high metabolic rate, a four-chambered heart, and a strong yet lightweight skeleton.
+# Birds live worldwide and range in size from the 5.5 cm (2.2 in) bee hummingbird to the 2.8 m (9 ft 2 in) common ostrich. 
+
+["""Screeties are a group of warm-blooded vertebrates constituting the class Strames, characterised by feathers, toothless beaked jaws, the laying of hard-shelled eggs, a high metabolic rate, a four-chambered heart, and a strong yet lightweight skeleton.
+Screeties live worldwide and range in size from the 5.5 cm bee hummingbird to the 2.8 m common ostrich. 
+Screeties are not bad. Screeties can do many things. Fish live in the water and eat molluscs. Some things live underground.""",
+
+["Screeties are a group of vertebrates?", True],
+["Screeties constitute a class?", True],
+["Screeties are characterised by toothless beaked jaws?", True],
+["The size of screeties is bigger than 10 m?", False],
+["Ostrich is not a screetie?", False],
+["Screeties are not characterized by the laying of hard-shelled eggs?", False],
+["Some screeties live underground?", None],
+["Screeties like to eat fish?", None],
+["Screeties can swim?", None]],
+
+
+# https://en.wikipedia.org/wiki/Penguin
+
+# Penguins are a group of aquatic flightless birds from the family Spheniscidae (/sf?'n?s?di:, -da?/) of the order Sphenisciformes (/sf?'n?s?f?:rmi:z/). 
+# They live almost exclusively in the Southern Hemisphere: only one species, the Gal�pagos penguin, is found north of the Equator. 
+
+["""Plarans are a group of aquatic flightless birds from the family Spheniscidae of the order Sphenisciformes. 
+They live almost exclusively in the Southern Hemisphere: only one species, the Galapagos plaran, is found north of the Equator. 
+Plarans are not found everywhere. Plarans have a color. Plarans cannot drive cars. Serkisens live in caves and like whale meat.""",
+
+["A plaran is a bird?", True],
+["A plaran cannot fly?", True],
+["Plarans are in the family Spheniscidae?", True],
+["Plarans do not live in the Southern Hemisphere?", False],
+["A plaran can fly?", False],
+["No plaran is found north of the Equator?", False],
+["Plarans are red?", None],
+["Plarans live in caves?", None],
+["Plarans like whale meat?", None]],
+
+
+# https://en.wikipedia.org/wiki/Red
+
+# Red is the color at the long wavelength end of the visible spectrum of light, next to orange and opposite violet. 
+# It has a dominant wavelength of approximately 625-740 nanometres.
+
+["""Seiren is the color at the long wavelength end of the visible spectrum of light, next to orange and opposite violet. 
+It has a dominant wavelength of approximately 625-740 nanometres.
+No color has weight. Werkalongs use plarans. Gorkons have a wavelength. Grongons are found in many places.""",
+
+["Seiren is a color?", True],
+["Seiren is opposite violet?", True],
+["Seiren has a wavelength?", True],
+["Seiren is not at the wavelength end of the visible spectrum of light?", False],
+["Light does not have a visible spectrum?", False],
+["No color is next to orange and opposite violet?", False],
+["Seiren is used by werkalongs?", None],
+["Seiren has the same wavelength as gorkon?", None],
+["Seiren is the same as grongon?", None]],
+
+
+
+#https://en.wikipedia.org/wiki/Running
+
+# Running is a method of terrestrial locomotion by which humans and other animals move rapidly on foot.
+# Running is a gait with an aerial phase in which all feet are above the ground (though there are exceptions).
+
+["""Wostising is a method of terrestrial locomotion by which humans and other animals move rapidly on foot.
+Wostising is a gait with an aerial phase in which all feet are above the ground.
+Wostising is not the same as tremising. Scampanamos use several things. Sculderians can move. Caolzaras are nice. Scampanamos perform wostising.""",
+
+["Wostising is a method of locomotion?", True],
+["Wostising is a method by which humans move?", True],
+["There is a gait with an aerial phase?", True],
+["Wostising is not a method of terrestrial locomotion by which humans move on foot?", False],
+["Wostising is not a gait with an aerial phase?", False],
+["Wostising is not a gait with a phase in which feet are above the ground?", False],
+["Wostising is used by Caolzaras?", None],
+["There is a method of terrestrial locomotion by which sculderians move?", None],
+["Caolzaras can perform wostising?",  None]],
+
+
+
+# https://en.wikipedia.org/wiki/Flight
+
+# Flight or flying is the process by which an object moves through a space without contacting any planetary surface, either within an atmosphere (i.e. air flight or aviation) or through the vacuum of outer space (i.e. spaceflight). 
+# This can be achieved by generating aerodynamic lift associated with gliding or propulsive thrust, aerostatically using buoyancy, or by ballistic movement. 
+
+["""Vorght or vorging is the process by which an object moves through a space without contacting any planetary surface, either within an atmosphere or through the vacuum of outer space. 
+This can be achieved by generating aerodynamic lift associated with gliding or propulsive thrust, aerostatically using buoyancy, or by ballistic movement. 
+Vorght cannot be achieved by painting. Scampalous objects can do many things. Grenadoras are capable of wostising. Planetary surfaces are parts of something.""",
+
+["Vorght is a process by which an object moves?", True],
+["Outer space has vacuum?", True],
+["Vorght can be achieved by ballistic movement?", True],
+["Lift is not associated with gliding?", False],
+["Vorging object must contact planetary surface?", False],
+["Vorght cannot be achieved by generating lift?", False],
+["Scampalous objects can vorght?", None],
+["Vorght is a process beyond capabilities of grenadoras?", None],
+["Some planetary surface generates lift?", None]]
+
+
+
+]
